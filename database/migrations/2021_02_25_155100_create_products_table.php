145e6a67d671e42f78c141ea76016631d5bdb963
Jakoffe <31653208+Jakoffe@users.noreply.github.com>
2021-02-26 12:53:48 +0100

    Update 2021_02_25_155100_create_products_table.php

--- database/migrations/2021_02_25_155100_create_products_table.php
+++ database/migrations/2021_02_25_155100_create_products_table.php
@@ -17,8 +17,8 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name');
             $table->float('price');
-            $table->float('stock');
-            $table->float('company-id');
+            $table->integer('stock');
+            $table->foreignId('company_id')->constrained();
             $table->timestamps();
         });
     }
